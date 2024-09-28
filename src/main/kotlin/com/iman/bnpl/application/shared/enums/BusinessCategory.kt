88fa9b9c349e2a7aeefff0af7ffd7b00730f0fe1
Iman Soltani <iman.soltani.j@gmail.com>
2024-09-28 16:54:20 +0330

    Modify BusinessCategory

--- src/main/kotlin/com/iman/bnpl/application/shared/enums/BusinessCategory.kt
+++ src/main/kotlin/com/iman/bnpl/application/shared/enums/BusinessCategory.kt
@@ -3,5 +3,7 @@ package com.iman.bnpl.application.shared.enums
 enum class BusinessCategory {
     CLOTHES,
     SUPER_MARKETS,
-    ACCESSORIES
+    ACCESSORIES,
+    HOME_KITCHEN,
+    BEAUTY_HEALTH
 }
